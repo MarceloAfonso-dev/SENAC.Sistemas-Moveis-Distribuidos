22a57fb9cb877c72b40b53b28c19cb0987ef6a44
Marcelo Afonso <133604815+MarceloAfonso-dev@users.noreply.github.com>
2025-02-12 22:26:02 -0300

    Add files via upload

---
+++ tipoNumerico.js
@@ -0,0 +1,25 @@
+const year = 1991;
+let delayInSeconds = 0.00016;
+let area = (16 * 3.14);
+let halfArea = area / 2;
+
+console.log(year);
+console.log(typeof year);
+
+let a = 10;     // decimal - default 
+let b = 0x10;   // hexadecimal
+let c = 0o10;   //octal
+let d = 0b10;   // binary
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let x = 9e3;
+let y = 123e-5;
+console.log(x);
+console.log(y);
+
+let z = 0x21;
+console.log(z);
